eb6d62401867eb01e0f9daf49614cb501aef4102
Robin <Robin@Aling-PC>
2017-03-01 17:05:42 +0800

    第1次把test1提交到本地仓库

---
+++ src/com/robin/servlet/Test3.java
@@ -0,0 +1,44 @@
+package com.robin.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/servlet/*")
+public class Test3 extends HttpServlet {
+
+	/**
+	 * The doGet method of the servlet. <br>
+	 *
+	 * This method is called when a form has its tag value method equals to get.
+	 * 
+	 * @param request the request send by the client to the server
+	 * @param response the response send by the server to the client
+	 * @throws ServletException if an error occurred
+	 * @throws IOException if an error occurred
+	 */
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.println("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\">");
+		out.println("<HTML>");
+		out.println("  <HEAD><TITLE>A Servlet</TITLE></HEAD>");
+		out.println("  <BODY>");
+		out.println("request.getRequestURI() "+request.getRequestURI()+"<br>");
+		out.println("request.getContextPath() "+request.getContextPath()+"<br>");
+		out.println("request.getServletPath() "+request.getServletPath()+"<br>");
+		out.println("request.getPathInfo() "+request.getPathInfo()+"<br>");
+		out.println("  </BODY>");
+		out.println("</HTML>");
+		out.flush();
+		out.close();
+	}
+
+}
